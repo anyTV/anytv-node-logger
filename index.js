81e0e775b80a0136d8cf752e99330a746ec12da5
Raven Lagrimas <rjlagrimas08@gmail.com>
2015-01-21 08:08:19 +0800

    Added package.json and .gitignore, remove log override

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var winston = require('winston'),
-    config = require(__dirname + '/../config/config'),
-    util = require(__dirname + '/../helpers/util'),
     logger;
 
 /*
@@ -23,17 +21,6 @@ winston.cli();
 switch (process.env['NODE_ENV']) {
     case 'testing' :
         logger = new (winston.Logger)();
-        break;
-
-    case 'custom_local' :
-        logger = new (winston.Logger)({
-            transports: [
-                new (winston.transports.Console)({
-                    level : 'verbose',
-                    colorize : true
-                })
-            ]
-        });
         break;
 
     case 'development' :
@@ -81,17 +68,5 @@ switch (process.env['NODE_ENV']) {
 }
 
 logger.cli();
-
-logger._log = logger.log;
-logger.log = function () {
-    var arg = arguments[0].split('-'),
-        logger_args = [arg[0]].concat(Array.prototype.slice.call(arguments, 1));
-
-    if (arg.pop() === 'log') {
-        util.log(arguments[1], arg.join('-'), Array.prototype.slice.call(arguments, 2).join(' '));
-    }
-
-    logger._log.apply(logger, logger_args);
-};
 
 module.exports = logger;
